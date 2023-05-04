7643572d83b235a12372f91ae0ec1af1913d52b8
Rashid Riaz <rashidriax0@gmail.com>
2023-05-04 05:05:17 +0500

    chore:redesign apps landing page

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+
+
+export const Button = ({label, onClick, type, className}) => {
+    return (
+        <button className={`btn text-white px-4 py-2 my-1 ${className}`} type={type}
+                style={{backgroundColor: '#452c63'}} onClick={onClick}>
+            {label}
+        </button>
+    )
+}
