81ca7b83bcff25ae48beb8ed7fc09171e451d254
maryam <maryaam2209@gmail.com>
2023-05-15 11:17:27 +0500

    15.5 frontend funct

--- src/panels/BrandManagerPanel/Campaigns/Campaigns.jsx
+++ src/panels/BrandManagerPanel/Campaigns/Campaigns.jsx
@@ -8,44 +8,101 @@ import { Home, People } from '@mui/icons-material';
 import '../../../Style/BrandManagerPanel/brandManagerDashboard/campaigns.css'
 
 const Campaigns = () => {
+ const [posts, setPosts] = useState([]);
  const [campaigns, setCampaigns] = useState([]);
+ const [subreddits, setSubreddits] = useState([]);
+ 
+ //get subreddit name from localhost, show given likes against subreddit names
 
+  //  useEffect(() => {
+  //   axios.get('http://127.0.0.1:8000/activecampaigns/')
+  //     .then(response => {
+  //       setCampaigns(response.data);
+  //       setSubreddits(response.hashtag)
+  //       console.log("sub is " + campaigns.hashtag);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
- useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        'https://oauth.reddit.com/r/apple/new.json?limit=100&fields=title',
-        {
-          headers: {
-            Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0MTI3NDA5LCJqdGkiOiIyNDQ5NTMxNzM4MjUzLU45U25PMTRHaTYyTzFoUmFtUmhIWld3cm1qWWRNUSIsImNpZCI6Ijc1OFlUT01OZ0U4UzA4MW5jSEJmNUEiLCJsaWQiOiJ0Ml92OWFyeTlvdCIsImFpZCI6InQyX3Y5YXJ5OW90IiwibGNhIjoxNjcyMjIzODM5MDAwLCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyJ9.hal1L4-snSsj-yfWhWi-HxRd8n1lTXJQu5U7sOJNxCuV-BHaTmHHlCja4-pRP9zbpaUx0gpVtTrsbJCF3K9kwaK7TmhXWiQfyp4nHOSdoJaVcnsK78vGy0mVF8DWpD0K2JOfgYCiLc3U4DXDCnbAS9W_vv566fgdkS3tYLM3gIdEOrRMH7dRiUKKc4Y9zVRw6Bo1Bl-keUZBQDQECybhNic_xahykNs6uM-KsGc1DGLdKHqrX6j1XvNRV5-NgqwDkbiIUGQHDaSUFagBfQT-WybEJbKm2NJd_aR2dYQgzlIioQKzsxE1jTuxlTaab4VAU8AwXkFiWns-P1CBEO67nQ',
-            'User-Agent': 'ChangeMeClient/0.1 by YourUsername'
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/activecampaigns/')
+      .then(response => {
+        setCampaigns(response.data);
+        setSubreddits(response.data.map(campaign => campaign.hashtag));
+        console.log(subreddits);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://oauth.reddit.com/r/apple/new.json?limit=100&fields=title',
+          {
+            headers: {
+              Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0MjEzNDUyLCJqdGkiOiIyNDQ5NTMxNzM4MjUzLTZXR2xSOG1fcnRvUURGcjdRWThuQURtOHZyVjNtdyIsImNpZCI6Ijc1OFlUT01OZ0U4UzA4MW5jSEJmNUEiLCJsaWQiOiJ0Ml92OWFyeTlvdCIsImFpZCI6InQyX3Y5YXJ5OW90IiwibGNhIjoxNjcyMjIzODM5MDAwLCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyJ9.ax4qqUajbzGdFohTweUNn-lwRRVshXDe7vNnZWLbi8aToBmPXGt4DKV5qFIclUIQsD3qr5bNl3yx74IUKszPtRO9vMNsQZmIEeKLd4xH4i_GcFqPgHEtpqjoe-fgyxDdBgTENZDPUqfeiTC-tUva7ecV2qxooKTb8t9lsHTUYbQiXz5oFL3G9oLxiBzaj9_vUsOA9tNvzJEaRuoRX9w1v7pn7K989TLOehDA0azmNsWhlmpeKornkwItY3LloIdNZEWGJ15aSUa_lZ9mXmvVrWtJrupjXgI70KD4dVW0LKt7M7-zqSaXnSBeuyxd4NOAJMRV0pD6E-irYZDHPncVVw',
+              'User-Agent': 'ChangeMeClient/0.1 by YourUsername'
+            }
           }
-        }
-      );
-    
-      const jsonData = response.data.data.children;
-      const postsArray = jsonData.map((post) => ({
-        title: post.data.title,
-        image: post.data.thumbnail ? post.data.thumbnail : 'https://i.pinimg.com/736x/10/a9/1b/10a91b37c6e5efb1cb18cebb1b4077ac.jpg',
-        likes: post.data.ups,
-        comments: post.data.num_comments,
-        author: post.author,
-        up: post.data.ups,
-        down: post.data.downs,
-        created: new Date(post.data.created_utc * 1000).toLocaleString(),
-        postType: post.data.post_hint,
-        subreddit: post.data.subreddit
-      }));
-      setCampaigns(postsArray);
-      console.log(postsArray);
+        );
+      
+        const jsonData = response.data.data.children;
+        const postsArray = jsonData
+          .filter(post => post.data.subreddit === subreddits && post.data.likes !== null)
+          .reduce((acc, post) => acc + post.data.likes, 0);
+          setPosts(postsArray);
+          console.log(`Subreddit "${subreddits}" has ${postsArray} total likes.`);
+  
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchData();
-}, []);
+
+
+//  useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get(
+//         'https://oauth.reddit.com/r/apple/new.json?limit=100&fields=title',
+//         {
+//           headers: {
+//             Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0MjEzNDUyLCJqdGkiOiIyNDQ5NTMxNzM4MjUzLTZXR2xSOG1fcnRvUURGcjdRWThuQURtOHZyVjNtdyIsImNpZCI6Ijc1OFlUT01OZ0U4UzA4MW5jSEJmNUEiLCJsaWQiOiJ0Ml92OWFyeTlvdCIsImFpZCI6InQyX3Y5YXJ5OW90IiwibGNhIjoxNjcyMjIzODM5MDAwLCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyJ9.ax4qqUajbzGdFohTweUNn-lwRRVshXDe7vNnZWLbi8aToBmPXGt4DKV5qFIclUIQsD3qr5bNl3yx74IUKszPtRO9vMNsQZmIEeKLd4xH4i_GcFqPgHEtpqjoe-fgyxDdBgTENZDPUqfeiTC-tUva7ecV2qxooKTb8t9lsHTUYbQiXz5oFL3G9oLxiBzaj9_vUsOA9tNvzJEaRuoRX9w1v7pn7K989TLOehDA0azmNsWhlmpeKornkwItY3LloIdNZEWGJ15aSUa_lZ9mXmvVrWtJrupjXgI70KD4dVW0LKt7M7-zqSaXnSBeuyxd4NOAJMRV0pD6E-irYZDHPncVVw',
+//             'User-Agent': 'ChangeMeClient/0.1 by YourUsername'
+//           }
+//         }
+//       );
+    
+//       const jsonData = response.data.data.children;
+//       const postsArray = jsonData.map((post) => ({
+//         title: post.data.title,
+//         image: post.data.thumbnail ? post.data.thumbnail : 'https://i.pinimg.com/736x/10/a9/1b/10a91b37c6e5efb1cb18cebb1b4077ac.jpg',
+//         likes: post.data.ups,
+//         comments: post.data.num_comments,
+//         author: post.author,
+//         up: post.data.ups,
+//         down: post.data.downs,
+//         created: new Date(post.data.created_utc * 1000).toLocaleString(),
+//         postType: post.data.post_hint,
+//         subreddit: post.data.subreddit
+//       }));
+//       setPosts(postsArray);
+//       console.log(postsArray);
+
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   fetchData();
+// }, []);
 
 
   return (
@@ -66,7 +123,32 @@ const Campaigns = () => {
 
 
     <div className="mainContainerC" style={{display: 'flex', flexWrap: "nowrap"}}> 
-      {campaigns.slice(0,20).map(item => {
+    {campaigns.slice(0,20).map(item => {
+        return (
+            <div className="subContainerC" >
+              <div>
+              <img className="imageC" src={item.image ? item.image : 'https://i.pinimg.com/736x/10/a9/1b/10a91b37c6e5efb1cb18cebb1b4077ac.jpg'} />
+              </div>
+
+              <Row className='mt-2'>
+                  <Col xs={12} sm={12} md={6} lg={7}>
+                      <h3 className='nameC'>{item.campaign_name.slice(0,18)}...</h3>
+                      <p className='influencersC'><People style={{height:"15px"}}/>{item.likes}</p>                      
+                  </Col>
+                  <Col xs={12} sm={12} md={6} lg={5} key={item.id}>
+                      <p className="hashtagC">#{item.hashtag}</p>
+                      {/* <div> <p className='typeC' style={{backgroundColor: item.postType === 'link' ? '#B47EE5' : 'green', }}>
+                        {item.postType ? item.postType : 'others'}</p>
+                      </div> */}
+                      <div><p className='typeC' style={{backgroundColor: item.campaign_type === 'periodic' ? '#B47EE5' : 'green', }}>
+                        {item.campaign_type ? item.campaign_type : 'others'}</p>
+                      </div>
+                  </Col>
+                </Row>
+                <p className='dateC'>{item.created}</p>
+            </div>
+        )})}
+      {/* {posts.slice(0,20).map(item => {
         return (
             <div className="subContainerC" >
               <div>
@@ -87,7 +169,7 @@ const Campaigns = () => {
                 </Row>
                 <p className='dateC'>{item.created}</p>
             </div>
-        )})}
+        )})} */}
     </div>
     </Row>
   );
@@ -107,16 +189,6 @@ export default Campaigns;
 //  const [campaigns, setCampaigns] = useState([]);
 
 
-//   useEffect(() => {
-//     axios.get('http://127.0.0.1:8000/activecampaigns/')
-//       .then(response => {
-//         setCampaigns(response.data);
-//         console.log(response.data);
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   }, []);
 
 //   return (
 //       <Row style={{border: '0.1px solid rgb(235, 232, 232)'}}>
@@ -169,3 +241,60 @@ export default Campaigns;
 // }
 
 // export default Campaigns;
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const [subreddits, setSubreddits] = useState([]);
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     const subData = await axios.get('http://127.0.0.1:8000/activecampaigns/');
+//     const subNames = subData.data.map(campaign => campaign.hashtag);
+//     setSubreddits(subNames);
+
+//     const likesBySub = await Promise.all(
+//       subNames.map(async (sub) => {
+//         const response = await axios.get(`https://oauth.reddit.com/r/${sub}/new.json?limit=100&fields=likes`);
+//         const jsonData = response.data.data.children;
+//         const postsArray = jsonData
+//           .filter(post => post.data.likes !== null)
+//           .reduce((acc, post) => acc + post.data.likes, 0);
+//         console.log(`Subreddit "${sub}" has ${postsArray} total likes.`);
+//         return { subreddit: sub, likes: postsArray };
+      // })
+      // );
+
+      // useEffect(() => {
+      //   const fetchData = async () => {
+      //     const likesArray = [];
+      //     for (const subreddit of subreddits) {
+      //       try {
+      //         const response = await axios.get(
+      //           `https://oauth.reddit.com/r/${subreddit}/new.json?limit=100&fields=likes`,
+      //           {
+      //             headers: {
+      //               Authorization: 'Bearer token',
+      //               'User-Agent': 'ChangeMeClient/0.1 by YourUsername'
+      //             }
+      //           }
+      //         );
+      //         const jsonData = response.data.data.children;
+      //         const likes = jsonData
+      //           .filter(post => post.data.likes !== null)
+      //           .reduce((acc, post) => acc + post.data.likes, 0);
+      //         likesArray.push({ subreddit, likes });
+      //         console.log(`Subreddit "${subreddit}" has ${likes} total likes.`);
+      //       } catch (error) {
+      //         console.error(error);
+      //       }
+      //     }
+      //     setLikes(likesArray);
+      //   };
+      //   fetchData();
+      // }, [subreddits]);
+      
