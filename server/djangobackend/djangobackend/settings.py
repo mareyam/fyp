dbde346c5284ad512b50bfa778ef81fe5d76558a
Marwa Khalid <77286253+marwa-khalid@users.noreply.github.com>
2023-06-23 22:09:37 +0500

    db online

--- server/djangobackend/djangobackend/settings.py
+++ server/djangobackend/djangobackend/settings.py
@@ -106,24 +106,23 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'Brandsense',
-#         'USER': 'postgres',
-#         'PASSWORD': 'brandsense',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ucezupix',
+        'USER': 'ucezupix',
+        'PASSWORD': 'tRobI9DODoXGbRZJDAebfW-CkrDsGKe4',
+        'HOST': 'lucky.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
